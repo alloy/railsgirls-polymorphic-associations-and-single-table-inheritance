4979d88c12d536546d6e7b87c7133a19bbcc6a4c
Eloy Durán <eloy.de.enige@gmail.com>
2014-12-14 00:12:51 +0100

    [STI] Add soccer and basketball teams and show in select popups.

--- app/models/soccer_event.rb
+++ app/models/soccer_event.rb
@@ -1,2 +1,24 @@
 class SoccerEvent < SportEvent
+  def self.teams
+    [
+      "ADO Den Haag",
+      "AZ",
+      "Ajax",
+      "Excelsior",
+      "FC Dordrecht",
+      "FC Groningen",
+      "FC Twente",
+      "FC Utrecht",
+      "Feyenoord",
+      "Go Ahead Eagles",
+      "Heracles Almelo",
+      "NAC Breda",
+      "PEC Zwolle",
+      "PSV",
+      "SC Cambuur",
+      "SC Heerenveen",
+      "Vitesse",
+      "Willem II"
+    ]
+  end
 end
